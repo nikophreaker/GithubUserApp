7f6c8eed4329be3c04484226de9cb8a94e0ba0a2
nikxphreaker <nikx449@gmail.com>
2020-08-07 10:53:45 +0700

    Just  my training lol

---
+++ app/src/main/java/com/example/githubuserapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.githubuserapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
